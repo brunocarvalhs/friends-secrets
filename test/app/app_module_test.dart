c4ecba664e43c3be177562bffa3670da5c0e3bb1
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2022-01-25 19:06:00 -0300

    implemetation test modules login

---
+++ test/app/app_module_test.dart
@@ -0,0 +1,21 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter_modular/flutter_modular.dart';
+// import 'package:friends_secrets/app/app_module.dart';
+// import 'package:friends_secrets/app/modules/login/presenter/stores/auth_store.dart';
+// import 'package:modular_test/modular_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // setUpAll(() async {
+  //   TestWidgetsFlutterBinding.ensureInitialized();
+  //   await Firebase.initializeApp();
+  //   initModule(AppModule());
+  // });
+
+  // group("AppModule - ", () {
+  //   test("get global instance AuthStore", () {
+  //     final result = Modular.get<AuthStore>();
+  //     expect(result, isA<AuthStore>());
+  //   });
+  // });
+}
---
+++ test/app/modules/login/domain/usecases/logount_test.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:friends_secrets/app/modules/login/domain/errors/errors.dart';
+import 'package:friends_secrets/app/modules/login/domain/repositories/login_repository.dart';
+import 'package:friends_secrets/app/modules/login/domain/usecases/logount.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'logount_test.mocks.dart';
+
+@GenerateMocks([
+  LoginRepository
+], customMocks: [
+  MockSpec<LoginRepository>(
+    as: #LoginRepositoryMock,
+    returnNullOnMissingStub: true,
+  ),
+  MockSpec<GoogleSignIn>(
+    as: #GoogleSignInMock,
+    returnNullOnMissingStub: true,
+  )
+])
+void main() {
+  final repository = MockLoginRepository();
+  final usecase = LogoutImpl(repository);
+  test('feat(login) - should verify if exist User Logged', () async {
+    when(repository.logout()).thenAnswer((_) async => const Right(unit));
+    var result = (await usecase()).fold((l) => null, (r) => r);
+    expect(result, unit);
+  });
+  test('feat(login) - should return null if user not logged', () async {
+    when(repository.logout()).thenAnswer((_) async => Left(ErrorLogout(message: '')));
+
+    var result = (await usecase()).fold(id, id);
+    expect(result, isA<ErrorLogout>());
+  });
+}
