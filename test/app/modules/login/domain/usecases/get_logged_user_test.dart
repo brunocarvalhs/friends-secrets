e3cf4ef8fd3365fff7d297a2d3589e7a2d64ca61
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2022-02-21 10:00:50 -0300

    Adapt code new version of backend

--- test/app/modules/login/domain/usecases/get_logged_user_test.dart
+++ test/app/modules/login/domain/usecases/get_logged_user_test.dart
@@ -23,7 +23,7 @@ void main() {
   final usecase = GetLoggedUserImpl(repository);
 
   test('feat(login) - should verify if exist User Logged', () async {
-    when(repository.loggedUser()).thenAnswer((_) async => const Right(UserModel(uuid: "")));
+    when(repository.loggedUser()).thenAnswer((_) async => const Right(UserModel(id: "")));
     var result = await usecase();
     expect(result.fold((l) => null, (r) => r), isA<LoggedUserInfo>());
   });
