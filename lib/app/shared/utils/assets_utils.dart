29958aac27396325895dbfda6d2876528e9cfaaf
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2022-03-18 16:46:18 -0300

    Update dir of assets

--- lib/app/shared/utils/assets_utils.dart
+++ lib/app/shared/utils/assets_utils.dart
@@ -1,4 +1,4 @@
 const String logo = "assets/images/logo.png";
 const String google = "assets/images/google.png";
-const String loginBackground = "assets/animated/lf30_dfgj520b.json";
+const String loginBackground = "assets/animated/lf20_q7tovsbz.json";
 const String loadingAnimated = "assets/animated/lf20_84tjzrve.json";
