228b21296f6534d152a591fcd997be7b612a8f60
brunocarvalhs <bruno.silva.carvalho@outlook.com.br>
2022-03-26 14:42:58 -0300

    Remove imports

--- lib/app/modules/notification/infra/repositories/notification_repository.dart
+++ lib/app/modules/notification/infra/repositories/notification_repository.dart
@@ -1,6 +1,4 @@
 import 'package:dartz/dartz.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:friends_secrets/app/core/infra/datasources/network_datasource.dart';
 import 'package:friends_secrets/app/modules/login/presenter/stores/auth_store.dart';
 import 'package:friends_secrets/app/modules/notification/domain/errors/errors.dart';
